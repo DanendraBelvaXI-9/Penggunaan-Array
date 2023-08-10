83edac910a46dc126064a23914393700d72100a4
DanendraBelvaXI-9 <141287028+DanendraBelvaXI-9@users.noreply.github.com>
2023-08-10 08:30:57 +0700

    Add files via upload

---
+++ Penggunaan Array_Danendra Belva A_XI-9.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+	//mendeklarasikan nilai variabel
+	int array1[5] = {7,6,9,1,16};
+	int array2[5];
+	int tambah,kurang,kali;
+	
+	//memberikan nilai pada elemen array 2
+	array2[0]=2;
+	array2[1]=3;
+	array2[2]=4;
+	//membuat persamaan matematika
+	tambah = array1[2]+array2[0];
+	kurang = array1[3]-array2[1];
+	kali = array1[4]*array2[2];
+	
+	//Menampilkan hasil persamaan
+	cout << "Array1[2] + Array2[0] = " << tambah<< endl;
+	cout << "Array1[3] - Array2[1] = " << kurang << endl;
+	cout << "Array1[4] * Array2[3] = " << kali<< endl;
+	
+	system ("PAUSE");
+	return 0;
+ }
